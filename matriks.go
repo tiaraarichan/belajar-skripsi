596ff6c3ceb76c764c2788e70f48130c6f899486
tiaraarichan <34361148+tiaraarichan@users.noreply.github.com>
2018-01-24 11:25:17 +0700

    membuat matriks.go
    kode ini belum berjalan dengan baik.

---
+++ matriks.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main(){
+        var b1 =[1][3]int{{1, 2, 3}}
+        var     b2 = [1][3]int{{4, 5, 6}}
+        var     b3 = [1][3]int{{7, 8, 9}}
+        //      fmt.Println(b1[1][1])
+
+
+for  i:=0; i< 2; i++ {
+  fmt.Println(b1,"\n" b2, "\n" b3)
+}
+}
